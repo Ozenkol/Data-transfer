1eb5b07ec0be3c36439ca896c9f40240f27fb668
Rechnoe <temirlantashen4@gmail.com>
2025-03-23 22:41:44 +0500

    v1

---
+++ preparation.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(*) FROM source_db.recommendation r
+LEFT JOIN target_db.users u ON r.user_id = u.id
+WHERE u.id IS NULL;
+
+SELECT COUNT(*) FROM source_db.recommendation r
+LEFT JOIN target_db.products p ON r.product_id = p.id
+WHERE p.id IS NULL;
